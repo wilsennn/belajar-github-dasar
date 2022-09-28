5e63b450c61e5a6a358f86a86ff8d844dc54d38c
wilsennn <wilsenfendrius@mhs.mdp.ac.id>
2022-09-28 10:58:28 +0700

    login
    login

---
+++ python test.py
@@ -0,0 +1,16 @@
+print ("||=======================||")
+print ("||        LOGIN          ||")
+print ("||=======================||")
+
+Username = input("Masukan Username: ")
+Password = input("Masukan Password: ")
+
+if Username == "Wilsennn":
+    if Password == "kunci123":
+        print("Selamat Datang di mana")
+    
+    else :
+        print("Password atau Username yang anda masukan salah ngab :) ")
+
+else :
+    print ("Akun Tidak Ditemukan")
